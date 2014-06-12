2754704bd5235528b78b4a41b3174ed39a45147d
Gavin McQuillan <gavin.mcquillan@gmail.com>
2014-06-12 16:08:55 -0700

    Adding more tests for has_perm

--- superperms/tests/test_decorators.py
+++ superperms/tests/test_decorators.py
@@ -2,7 +2,7 @@ import json
 
 from django.contrib.auth.models import User
 from django.utils.unittest import TestCase
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 
 from superperms.orgs import decorators
 from superperms.orgs.exceptions import (
@@ -55,8 +55,17 @@ class FakeClient(object):
         return self._gen_req(view_func, data, headers, **kwargs)
 
 
-@decorators.has_perm('anus')
+#
+## These are test functions wrapped in decorators.
+###
+
+@decorators.has_perm('derp')
 def _fake_view_no_perm_name(request):
+    return HttpResponse()
+
+
+@decorators.has_perm('can_invite_member')
+def _fake_invite_user(request):
     return HttpResponse()
 
 
@@ -79,13 +88,19 @@ class TestDecorators(TestCase):
             email='fake_viewer@asdf.com'
         )
         self.fake_owner_org_user = OrganizationUser.objects.create(
-            user=self.fake_owner, organization=self.fake_org
+            user=self.fake_owner,
+            organization=self.fake_org,
+            role_level=ROLE_OWNER
         )
         self.fake_member_org_user = OrganizationUser.objects.create(
-            user=self.fake_member, organization=self.fake_org
+            user=self.fake_member,
+            organization=self.fake_org,
+            role_level=ROLE_MEMBER
         )
         self.fake_viewer_org_user = OrganizationUser.objects.create(
-            user=self.fake_viewer, organization=self.fake_org
+            user=self.fake_viewer,
+            organization=self.fake_org,
+            role_level=ROLE_VIEWER
         )
 
     def tearDown(self):
@@ -98,6 +113,28 @@ class TestDecorators(TestCase):
     ## Test has_perm in various permutations.
     ###
 
+    def test_has_perm_w_no_org(self):
+        """We should return BadRequest if there's no org."""
+        self.client.user = User.objects.create(username='f', email='d@d.com')
+        resp = self.client.post(
+            _fake_view_no_perm_name,
+            {'organization_id': 0},
+            user=self.client.user
+        )
+
+        self.assertEqual(resp.__class__, HttpResponseBadRequest)
+
+    def test_has_perm_user_not_in_org(self):
+        """We should reject requests from a user not in this org."""
+        self.client.user = User.objects.create(username='f', email='d@d.com')
+        self.assertRaises(
+            UserNotInOrganization,
+            self.client.post,
+            _fake_view_no_perm_name,
+            {'organization_id': self.fake_org.pk},
+            user=self.client.user
+        )
+
     def test_has_perm_w_no_perm_name(self):
         """Default to false if an undefined perm is spec'ed"""
         self.client.user = self.fake_member
@@ -108,6 +145,17 @@ class TestDecorators(TestCase):
             {'organization_id': self.fake_org.pk},
             user=self.fake_member
         )
+
+    def test_has_perm_good_case(self):
+        """Test that we actually allow people through."""
+        self.client.user = self.fake_owner
+        resp = self.client.post(
+            _fake_invite_user,
+            {'organization_id': self.fake_org.pk},
+            user = self.fake_owner
+        )
+
+        self.assertEqual(resp.__class__, HttpResponse)
 
     #
     ## Test boolean functions for permission logic.
