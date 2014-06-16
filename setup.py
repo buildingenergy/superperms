6b0b4e1f34fc84ed0c0703235d2de53c431fc1b8
Aleck Landgraf <aleck.landgraf@buildingenergy.com>
2014-06-16 15:35:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='superperms',
-    version='0.0.1',
+    version='0.2.2',
     description='Flexible Permissions for Django.',
     long_description=open('README.md').read(),
     author='Gavin McQuillan',
